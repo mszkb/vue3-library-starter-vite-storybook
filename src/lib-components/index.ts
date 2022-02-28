bbc5675d0ec3ce4d42b196d088ae156bdc470a70
mszkb <me@mszkb.de>
2022-02-28 09:23:23 +0100

    removing weird files, simplify exporting

--- src/lib-components/index.ts
+++ src/lib-components/index.ts
@@ -1,16 +1,3 @@
-import type { App, Plugin } from 'vue';
+import TestComponent from './TestComponent/TestComponent.vue'
 
-import TestComponent from './TestComponent';
-
-// install
-const install = (app: App): void => {
-    app.use(TestComponent);
-};
-
-export default { install };
-
-export {
-    TestComponent
-}
-
-export * from './TestComponent';
+export { TestComponent }
